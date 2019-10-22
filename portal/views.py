651c41ce3e31298feb8c2b21eca9b0a2bdfd1d69
renatojobal <renatojobal@gmail.com>
2019-10-22 16:59:00 -0500

    Creando la views y cmabiando la url de page a portal

--- portal/views.py
+++ portal/views.py
@@ -1,3 +1,5 @@
-from django.shortcuts import render
-
-# Create your views here.
+from django.shortcuts import render_to_response
+ 
+# View principal
+def index(request):
+    return render_to_response('page/index.html')
