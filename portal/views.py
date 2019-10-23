8aa914aae2d72e750a3afdb76a8dfaec83aaf870
renatojobal <renatojobal@gmail.com>
2019-10-23 18:22:52 -0500

    Eliminando resagos ded la app page (depreciada)

--- portal/views.py
+++ portal/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render_to_response
  
 # View principal
 def index(request):
-    return render_to_response('page/index.html')
+    return render_to_response('portal/index.html')
