5bb5fdc8735d596fbf33bcf74f621101ec99292f
renatojobal <renatojobal@gmail.com>
2019-10-28 22:06:23 -0500

    Cambiando la vista y populé la base de datos

--- portal/views.py
+++ portal/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render_to_response
 from portal import models
- 
+
+
+
 # View principal
 def index(request):
-    data_base_data =  {'data':'HOlaa'}
+    
+    # Aquí vamos a cargar los datos desde la base de datos
+
+    query = models.Portal.objects.filter(name="Viayestick")[0]
+    
+
+    data_base_data =  {'data':'Holaa'}
     return render_to_response('portal/index.html', context=data_base_data)
